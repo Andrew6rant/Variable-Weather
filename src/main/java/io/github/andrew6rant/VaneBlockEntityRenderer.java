e0d5c704164205a951d61fc7684ed2d6e1ae8649
Andrew Grant <andrew.grant@mail.rit.edu>
2023-06-12 23:05:40 -0400

    split spinner and platform in model

--- src/main/java/io/github/andrew6rant/VaneBlockEntityRenderer.java
+++ src/main/java/io/github/andrew6rant/VaneBlockEntityRenderer.java
@@ -12,30 +12,33 @@ import net.minecraft.util.math.MathHelper;
 
 public class VaneBlockEntityRenderer implements BlockEntityRenderer<VaneBlockEntity> {
     private final ModelPart platform;
+    private final ModelPart spinner;
     public VaneBlockEntityRenderer(BlockEntityRendererFactory.Context ctx) {
         ModelData platformData = new ModelData();
+        ModelData spinnerData = new ModelData();
         ModelPartData platformPartData = platformData.getRoot();
+        ModelPartData spinnerPartData = spinnerData.getRoot();
 
         // I had to make this manually, and it took me ages lol
-        platformPartData.addChild("head", ModelPartBuilder.create().uv(0, 0).cuboid(
+        spinnerPartData.addChild("head", ModelPartBuilder.create().uv(0, 0).cuboid(
                 6f, 15f, 3f,
                 4f, 6f, 3f), ModelTransform.NONE);
-        platformPartData.addChild("bill", ModelPartBuilder.create().uv(14, 0).cuboid(
+        spinnerPartData.addChild("bill", ModelPartBuilder.create().uv(14, 0).cuboid(
                 6f, 17f, 1f,
                 4f, 2f, 2f), ModelTransform.NONE);
-        platformPartData.addChild("body", ModelPartBuilder.create().uv(0, 9).cuboid(
+        spinnerPartData.addChild("body", ModelPartBuilder.create().uv(0, 9).cuboid(
                 5f, 11f, 5f,
                 6f, 6f, 8f), ModelTransform.NONE);
-        platformPartData.addChild("left_wing", ModelPartBuilder.create().uv(28, 13).cuboid(
+        spinnerPartData.addChild("left_wing", ModelPartBuilder.create().uv(28, 13).cuboid(
                 4f, 13f, 6f,
                 1f, 4f, 6f), ModelTransform.NONE);
-        platformPartData.addChild("right_wing", ModelPartBuilder.create().uv(28, 13).cuboid(
+        spinnerPartData.addChild("right_wing", ModelPartBuilder.create().uv(28, 13).cuboid(
                 11f, 13f, 6f,
                 1f, 4f, 6f), ModelTransform.NONE);
-        platformPartData.addChild("frill", ModelPartBuilder.create().uv(44, 21).cuboid(
+        spinnerPartData.addChild("frill", ModelPartBuilder.create().uv(44, 21).cuboid(
                 8f, 15f, 2f,
                 0f, 8f, 3f), ModelTransform.NONE);
-        platformPartData.addChild("arrow", ModelPartBuilder.create().uv(0, 33).cuboid(
+        spinnerPartData.addChild("arrow", ModelPartBuilder.create().uv(0, 33).cuboid(
                 8f, 6f, -3f,
                 0f, 5f, 22f), ModelTransform.NONE);
         platformPartData.addChild("east-west", ModelPartBuilder.create().uv(32, 0).cuboid(
@@ -47,17 +50,20 @@ public class VaneBlockEntityRenderer implements BlockEntityRenderer<VaneBlockEnt
         platformPartData.addChild("stand", ModelPartBuilder.create().uv(0, 0).cuboid(
                 7f, 0f, 7f,
                 2f, 6f, 2f), ModelTransform.NONE);
+
         this.platform = platformPartData.createPart(64, 32);
-        //this.platform.setPivot(0f, 0f, 0f);
+        this.spinner = spinnerPartData.createPart(64, 32);
+        this.spinner.setPivot(8f, 0f, 8f);
     }
 
     @Override
     public void render(VaneBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         if(entity.hasWorld()) {
             //float correctedTicks = (float) ((entity.getWorld().getTime() % 314.15) + tickDelta);
-            //this.platform.yaw = correctedTicks * 0.05f;
+            //this.spinner.yaw = correctedTicks * 0.05f;
         }
         VertexConsumer consumer = vertexConsumers.getBuffer(RenderLayer.getEntityCutout(new Identifier("variable-weather", "textures/entity/vane_debug.png")));
         this.platform.render(matrices, consumer, light, overlay);
+        this.spinner.render(matrices, consumer, light, overlay);
     }
 }
