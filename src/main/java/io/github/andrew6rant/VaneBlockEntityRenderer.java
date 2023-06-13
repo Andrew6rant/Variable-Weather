86fd893a888e03c201454f7763bdb7a544ffe6b8
Andrew Grant <andrew.grant@mail.rit.edu>
2023-06-12 23:10:35 -0400

    fix model offsets

--- src/main/java/io/github/andrew6rant/VaneBlockEntityRenderer.java
+++ src/main/java/io/github/andrew6rant/VaneBlockEntityRenderer.java
@@ -21,26 +21,27 @@ public class VaneBlockEntityRenderer implements BlockEntityRenderer<VaneBlockEnt
 
         // I had to make this manually, and it took me ages lol
         spinnerPartData.addChild("head", ModelPartBuilder.create().uv(0, 0).cuboid(
-                6f, 15f, 3f,
+                -2f, 15f, -5f,
                 4f, 6f, 3f), ModelTransform.NONE);
         spinnerPartData.addChild("bill", ModelPartBuilder.create().uv(14, 0).cuboid(
-                6f, 17f, 1f,
+                -2f, 17f, -7f,
                 4f, 2f, 2f), ModelTransform.NONE);
         spinnerPartData.addChild("body", ModelPartBuilder.create().uv(0, 9).cuboid(
-                5f, 11f, 5f,
+                -3f, 11f, -3f,
                 6f, 6f, 8f), ModelTransform.NONE);
         spinnerPartData.addChild("left_wing", ModelPartBuilder.create().uv(28, 13).cuboid(
-                4f, 13f, 6f,
+                -4f, 13f, -2f,
                 1f, 4f, 6f), ModelTransform.NONE);
         spinnerPartData.addChild("right_wing", ModelPartBuilder.create().uv(28, 13).cuboid(
-                11f, 13f, 6f,
+                3f, 13f, -2f,
                 1f, 4f, 6f), ModelTransform.NONE);
         spinnerPartData.addChild("frill", ModelPartBuilder.create().uv(44, 21).cuboid(
-                8f, 15f, 2f,
+                0f, 15f, -6f,
                 0f, 8f, 3f), ModelTransform.NONE);
         spinnerPartData.addChild("arrow", ModelPartBuilder.create().uv(0, 33).cuboid(
-                8f, 6f, -3f,
+                0f, 6f, -11f,
                 0f, 5f, 22f), ModelTransform.NONE);
+
         platformPartData.addChild("east-west", ModelPartBuilder.create().uv(32, 0).cuboid(
                 0f, 2f, 8f,
                 16f, 5f, 0f), ModelTransform.NONE);
@@ -59,8 +60,8 @@ public class VaneBlockEntityRenderer implements BlockEntityRenderer<VaneBlockEnt
     @Override
     public void render(VaneBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         if(entity.hasWorld()) {
-            //float correctedTicks = (float) ((entity.getWorld().getTime() % 314.15) + tickDelta);
-            //this.spinner.yaw = correctedTicks * 0.05f;
+            float correctedTicks = (float) ((entity.getWorld().getTime() % 314.15) + tickDelta);
+            this.spinner.yaw = correctedTicks * 0.05f;
         }
         VertexConsumer consumer = vertexConsumers.getBuffer(RenderLayer.getEntityCutout(new Identifier("variable-weather", "textures/entity/vane_debug.png")));
         this.platform.render(matrices, consumer, light, overlay);
